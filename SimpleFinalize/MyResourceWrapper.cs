3841415a2b1f0a6f1084e7bd0b0d1020c0e7fc72
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-06 00:19:11 +0300

    Building Finalizable Objects

---
+++ SimpleFinalize/MyResourceWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFinalize
+{
+	internal class MyResourceWrapper
+	{
+		// Compile-time error!
+		// protected override void Finalize() { }
+
+		// Clean up unmanaged resources here.
+		// Beep when destroyed (testing purposes only!)
+		~MyResourceWrapper() => Console.Beep();
+	}
+}
